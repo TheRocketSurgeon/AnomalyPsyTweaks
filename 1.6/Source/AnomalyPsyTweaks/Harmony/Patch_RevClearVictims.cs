ad2501fd4eea3a826f444a6ce738dc70457d4811
TheRocketSurgeon <9639394+TheRocketSurgeon@users.noreply.github.com>
2025-07-12 12:13:16 -0400

    hypnosis victims not being cleared

--- 1.6/Source/AnomalyPsyTweaks/Harmony/Patch_RevClearVictims.cs
+++ 1.6/Source/AnomalyPsyTweaks/Harmony/Patch_RevClearVictims.cs
@@ -18,35 +18,40 @@ namespace AnomalyPsyTweaks
         // does not check settings so APT revenant hediffs are still cleaned up if settings get changed mid event
         public static bool Prefix(CompRevenant __instance)
         {
+            Log.Message("APT: clearing revenant victims");
             if (__instance.revenantVictims.NullOrEmpty())
             {
                 return true;
             }
             foreach (Pawn revenantVictim in __instance.revenantVictims)
             {
+                Log.Message("APT: checking " + revenantVictim.Named("NAME"));
                 Hediff APT_hediffFailedHypno = revenantVictim?.health?.hediffSet?.GetFirstHediffOfDef(APT_DefOf.APT_FailedRevenantHypnosis);
                 if (APT_hediffFailedHypno != null)
                 {
+                    Log.Message(revenantVictim.Named("NAME") + " has failed hypnosis. attempting removal");
                     revenantVictim.health.RemoveHediff(APT_hediffFailedHypno);
                 }
                 Hediff APT_hediffMildHypno = revenantVictim?.health?.hediffSet?.GetFirstHediffOfDef(APT_DefOf.APT_MildRevenantHypnosis);
                 if (APT_hediffMildHypno != null)
                 {
+                    Log.Message(revenantVictim.Named("NAME") + " has partial hypnosis. attempting removal");
                     revenantVictim.health.RemoveHediff(APT_hediffMildHypno);
                 }
                 Hediff APT_hediffSevereHypno = revenantVictim?.health?.hediffSet?.GetFirstHediffOfDef(APT_DefOf.APT_SevereRevenantHypnosis);
                 if (APT_hediffSevereHypno != null)
                 {
+                    Log.Message(revenantVictim.Named("NAME") + " has severe hypnosis. attempting removal");
                     revenantVictim.health.AddHediff(APT_DefOf.APT_HypnoticShock);
                     revenantVictim.health.RemoveHediff(APT_hediffSevereHypno);
                 }
                 Hediff APT_hediffExtremeHypno = revenantVictim?.health?.hediffSet?.GetFirstHediffOfDef(APT_DefOf.APT_ExtremeRevenantHypnosis);
                 if (APT_hediffExtremeHypno != null)
                 {
+                    Log.Message(revenantVictim.Named("NAME") + " has extreme hypnosis. attempting removal");
                     revenantVictim.health.AddHediff(APT_DefOf.APT_ExtremeHypnoticShock);
                     revenantVictim.health.RemoveHediff(APT_hediffExtremeHypno);
                 }
-                return true;
             }
             return true;
         }
